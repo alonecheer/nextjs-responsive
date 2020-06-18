f79849f508678724784da7cbfe0b5d4d9f028e01
alonecheer <alonecheer@hotmail.com>
2020-06-18 18:05:37 +0700

    data table input data

--- pages/index.js
+++ pages/index.js
@@ -1,96 +1,65 @@
 import React, { useState, useEffect } from "react";
-import { Layout, Menu } from "antd";
-import {
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
-import ContentComponent from '../src/component/Content'
-const { Header, Content, Footer, Sider } = Layout;
+import axios from 'axios';
+import { Table } from 'antd';
 import styled from "styled-components";
 const StyledWrapper = styled.div`
-  .container {
-    max-width: 100%;
-    /*margin: 0 auto;*/
-    background-color: burlywood;
-  }
-  .main {
-    width: 320px;
-    height: 60px;
-    background-color: red;
-    float: left;
-  }
-  .menu1 {
-    width: 320px;
-    height: 60px;
-    background-color: blue;
-    float: left;
-  }
 
-  #components-layout-demo-responsive .logo {
-    height: 32px;
-    background: rgba(255, 255, 255, 0.2);
-    margin: 16px;
-  }
-
-  .site-layout-sub-header-background {
-    background: #fff;
-  }
-
-  .site-layout-background {
-    background: #fff;
-  }
 `;
 const Index = () => {
+  const [history, setHistoty] = useState([])
+  console.log('data =',history)
+  const wholeArray = Object.keys(history).map(key => history[key]);
+  console.log(wholeArray);
+  const getForm001Bysid = async () => {
+    var found = await axios.get(
+      `http://localhost:3001/form001/6035512021`
+    )
+    //console.log('found = ', found.data)
+    setHistoty(found.data)
+    
+    }
+  useEffect(() => {
+    getForm001Bysid()
+  }, [])
+  const columns = [
+    {
+      title: 'Order_id',
+      dataIndex: 'order_id',
+      key: 'order_id',
+      render: text => <a>{text}</a>,
+    },
+    {
+      title: 'o_date',
+      dataIndex: 'o_date',
+      key: 'o_date',
+      responsive: ['md'],
+    },
+    {
+      title: 'o_projectname',
+      dataIndex: 'o_projectname',
+      key: 'o_projectname',
+      responsive: ['lg'],
+    },
+  ];
+  
+  const data = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+    },
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+    },
+    
+  ];
   return (
     <StyledWrapper>
-      <div className="container">
-        <Layout>
-          <Sider
-            breakpoint="lg"
-            collapsedWidth="0"
-            onBreakpoint={(broken) => {
-              console.log(broken);
-            }}
-            onCollapse={(collapsed, type) => {
-              console.log(collapsed, type);
-            }}
-          >
-            <div className="logo" />
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
-              <Menu.Item key="1" icon={<UserOutlined />}>
-                nav 1
-              </Menu.Item>
-              <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-                nav 2
-              </Menu.Item>
-              <Menu.Item key="3" icon={<UploadOutlined />}>
-                nav 3
-              </Menu.Item>
-              <Menu.Item key="4" icon={<UserOutlined />}>
-                nav 4
-              </Menu.Item>
-            </Menu>
-          </Sider>
-          <Layout>
-            <Header
-              className="site-layout-sub-header-background"
-              style={{ padding: 0 }}
-            />
-            <Content style={{ margin: "24px 16px 0" }}>
-              <div
-                className="site-layout-background"
-                style={{ padding: 24, minHeight: 360 }}
-              >
-                <ContentComponent></ContentComponent>
-              </div>
-            </Content>
-            <Footer style={{ textAlign: "center" }}>
-              Ant Design ©2018 Created by Ant UED
-            </Footer>
-          </Layout>
-        </Layout>
-      </div>
+        <Table columns={columns} dataSource={history} />
     </StyledWrapper>
   );
 };
--- src/component/Content.js
+++ src/component/Content.js
@@ -10,21 +10,23 @@ const StyledWrapper = styled.div`
   max-width: 960px;
   margin: 0 auto;
   table {
-  border-collapse: collapse;
-  width: 100%;
-}
+    border-collapse: collapse;
+    width: 100%;
+  }
 
-th, td {
-  text-align: left;
-  padding: 8px;
-  font-size: 15px;
-}
+  th,
+  td {
+    text-align: left;
+    padding: 8px;
+    font-size: 15px;
+  }
 
-tr:nth-child(even) {background-color: #f2f2f2;}
+  tr:nth-child(even) {
+    background-color: #f2f2f2;
+  }
 
-input {
-    
-    border: 1px solid black ;
+  input {
+    border: 1px solid black;
     /*padding: 10px;*/
     /*width: 100%;*/
     /*background-color:#f0f2f5;*/
@@ -38,52 +40,108 @@ input {
 const ContentComponent = () => {
   return (
     <StyledWrapper>
-   <div style={{overflowX:'auto'}}>
-  <table>
-    <tr>
-      <th>ลำดับ</th>
-      <th>รายการ</th>
-      <th>จำนวน</th>
-      <th>หน่วย</th>
-      <th>ราคา/หน่วย</th>
-    </tr>
-    <tr>
-      <td>1</td>
-      <td><input type="text" name='l_detail_1'></input></td>
-      <td><input type="text" name='l_amount_1'></input></td>
-      <td><input type="text" name='l_unit_1'></input></td>
-      <td><input type="text" name='l_priceunit_1'></input></td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td><input type="text" name='l_detail_2'></input></td>
-      <td><input type="text" name='l_amount_2'></input></td>
-      <td><input type="text" name='l_unit_2'></input></td>
-      <td><input type="text" name='l_priceunit_1'></input></td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td><input type="text" name='l_detail_3'></input></td>
-      <td><input type="text" name='l_amount_3'></input></td>
-      <td><input type="text" name='l_unit_3'></input></td>
-      <td><input type="text" name='l_priceunit_1'></input></td>
-    </tr>
-    <tr>
-      <td>4</td>
-      <td><input type="text" name='l_detail_4'></input></td>
-      <td><input type="text" name='l_amount_4'></input></td>
-      <td><input type="text" name='l_unit_4'></input></td>
-      <td><input type="text" name='l_priceunit_1'></input></td>
-    </tr>
-    <tr>
-      <td>5</td>
-      <td><input type="text" name='l_detail_5'></input></td>
-      <td><input type="text" name='l_amount_5'></input></td>
-      <td><input type="text" name='l_unit_5'></input></td>
-      <td><input type="text" name='l_priceunit_1'></input></td>
-    </tr>
-  </table>
-</div>
+      <div style={{ overflowX: "auto" }}>
+        <table>
+          <tr>
+            <th>ลำดับ</th>
+            <th>รายการ</th>
+            <th>จำนวน</th>
+            <th>หน่วย</th>
+            <th>ราคา/หน่วย</th>
+          </tr>
+          <tr>
+            <td>
+              1
+              <input
+                name="sid"
+                value={username}
+                disabled={true}
+                type="hidden"
+                ref={register}
+              ></input>
+              <input
+                name="order_id"
+                value={order_id}
+                disabled={true}
+                type="hidden"
+                ref={register}
+              ></input>
+            </td>
+            <td>
+              <input ref={register} type="text" name="l_detail_1"></input>
+            </td>
+            <td>
+              <input ref={register} type="text" name="l_amount_1"></input>
+            </td>
+            <td>
+              <input ref={register} type="text" name="l_unit_1"></input>
+            </td>
+            <td>
+              <input ref={register} type="text" name="l_priceunit_1"></input>
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>
+              <input ref={register} type="text" name="l_detail_2"></input>
+            </td>
+            <td>
+              <input ref={register} type="text" name="l_amount_2"></input>
+            </td>
+            <td>
+              <input ref={register} type="text" name="l_unit_2"></input>
+            </td>
+            <td>
+              <input ref={register} type="text" name="l_priceunit_1"></input>
+            </td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>
+              <input ref={register} type="text" name="l_detail_3"></input>
+            </td>
+            <td>
+              <input ref={register} type="text" name="l_amount_3"></input>
+            </td>
+            <td>
+              <input ref={register} type="text" name="l_unit_3"></input>
+            </td>
+            <td>
+              <input ref={register} type="text" name="l_priceunit_1"></input>
+            </td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>
+              <input ref={register} type="text" name="l_detail_4"></input>
+            </td>
+            <td>
+              <input ref={register} type="text" name="l_amount_4"></input>
+            </td>
+            <td>
+              <input ref={register} type="text" name="l_unit_4"></input>
+            </td>
+            <td>
+              <input ref={register} type="text" name="l_priceunit_1"></input>
+            </td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>
+              <input ref={register} type="text" name="l_detail_5"></input>
+            </td>
+            <td>
+              <input ref={register} type="text" name="l_amount_5"></input>
+            </td>
+            <td>
+              <input ref={register} type="text" name="l_unit_5"></input>
+            </td>
+            <td>
+              <input ref={register} type="text" name="l_priceunit_1"></input>
+            </td>
+          </tr>
+        </table>
+      </div>
     </StyledWrapper>
   );
 };
