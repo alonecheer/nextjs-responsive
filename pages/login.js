6dac6b526201cc3002ad533eae3f009018d7e903
alonecheer <alonecheer@hotmail.com>
2020-07-10 23:05:12 +0700

    flex warp

--- pages/login.js
+++ pages/login.js
@@ -1,23 +1,15 @@
 import React, { useEffect, useState } from "react";
 import '../public/static/pages/login.css'
-import styled from "styled-components";
-const StyledWrapper = styled.div`
-  
-`;
+
 const Login = () => {
   return (
-    <StyledWrapper>
+    <div className="limiter">
       <div className="container-login100">
-        <div className="wrap-login100">
-
-          <div class="login100-pic js-tilt" data-tilt>
-            <img src="static/images/img-01.png" alt="IMG"></img>
-          </div>
-          
-          
-        </div>
+      <div class="wrap-login100">
+        
       </div>
-    </StyledWrapper>
+      </div>
+    </div>
   );
 };
 
